9257374194d6e92362e69b8c427eb4acc9425e98
T3sultan <incognito.soul011@gmail.com>
2020-12-07 00:40:38 +0600

    Change greeting method

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello React Native ");
-        greeting("Tipu Sultan");
+        greeting("Git");
     }
     public static void greeting(String name){
-        System.out.println("Wellcome" +name);
+        System.out.println("Bye Bye" +name);
     }
 }
