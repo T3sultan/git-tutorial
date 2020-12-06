9c148d1ffb4d692985528a40125bc06f36168aa8
T3sultan <incognito.soul011@gmail.com>
2020-12-07 00:24:17 +0600

    Added a new mothed called a greeting to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello React Native ");
+        greeting("Tipu Sultan");
+    }
+    public static void greeting(String name){
+        System.out.println("Wellcome" +name);
     }
 }
